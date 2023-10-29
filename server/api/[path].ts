cd26f409aff015bac228e10d9c2435bdab2f6678
ishah <ishah@intervale.ru>
2023-10-30 01:18:42 +0300

    bugfix

--- server/api/[path].ts
+++ server/api/[path].ts
@@ -1,11 +1,8 @@
 import { articlesPagePath, baseApiPath } from "~/constants";
-import { useArticleStore } from "~/store";
 
 export default defineEventHandler(async (event) => {
-  const store = useArticleStore();
   const path = event.context?.params?.path;
   if (!path) {
-    store.setErrorMessage();
     return;
   }
   const url = `${baseApiPath}${path === articlesPagePath ? "" : path}`;
@@ -13,7 +10,6 @@ export default defineEventHandler(async (event) => {
   try {
     return await $fetch(url);
   } catch {
-    store.setErrorMessage();
     return null;
   }
 });
